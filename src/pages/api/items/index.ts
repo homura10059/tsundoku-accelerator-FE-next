9d5e1e4d2490e5ea40323b29ac86518b3868d5f6
o-hayato <preasper0+github@gmail.com>
2020-12-29 12:47:05 +0900

    :recycle: add log

--- src/pages/api/items/index.ts
+++ src/pages/api/items/index.ts
@@ -5,8 +5,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const {
     query: { url },
   } = req
+  console.log(`url: ${url}`)
 
   const urls = await getItems(typeof url === 'string' ? [url] : url)
+  console.log(`urls: ${urls}`)
 
   res.status(200).json({ urls })
 }
