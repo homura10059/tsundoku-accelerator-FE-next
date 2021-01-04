09d299bc5379a8c75d408c357c60d962a082d83c
o-hayato <preasper0+github@gmail.com>
2021-01-04 13:42:25 +0900

    add items command

---
+++ schedulers/items/update.js
@@ -0,0 +1,9 @@
+const fetch = require('node-fetch')
+// レスポンスは必要ないのであえてawaitしない
+const port = ENV['PORT'] || '3000'
+const apiPath = '/api/items'
+const url =
+  port === '3000'
+    ? `http://localhost:${port}${apiPath}`
+    : `https://tsundoku-accelerator.herokuapp.com${apiPath}`
+fetch(url, { method: 'POST' })
