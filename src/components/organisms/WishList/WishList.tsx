fb654ecb07a778aa34094dad8aaa4aa331513312
o-hayato <preasper0+github@gmail.com>
2021-02-12 14:46:51 +0900

    fix list css

--- src/components/organisms/WishList/WishList.tsx
+++ src/components/organisms/WishList/WishList.tsx
@@ -1,32 +1,28 @@
 import React from 'react'
 import Router from 'next/router'
-import { WishList as WishListModel } from '../../../domain/model/WishList'
 import styled from 'styled-components'
 import { format } from 'date-fns'
+import { WishList as WishListProps } from '@prisma/client'
 
-export type Props = WishListModel
+export type Props = WishListProps
 
-const Wrapper = styled.article`
-  background-color: ${({ theme }) => theme.colors.secondary.light};
-  padding: 0.5rem;
-`
-const Title = styled.h1``
 const Area = styled.a``
+const Title = styled.p``
+const UpdateAt = styled.p``
 
 const WishList: React.FC<Props> = (wishList) => {
   const onClick = () =>
     Router.push('/wishList/[id]', `/wishList/${wishList.id}`)
-  const scrapedAt =wishList.scrapedAt ? format(new Date(wishList.scrapedAt * 1000), 'yyyy/MM/dd HH:mm:ss') : '-'
+  const scrapedAt = wishList.scrapedAt
+    ? format(new Date(wishList.scrapedAt * 1000), 'yyyy/MM/dd HH:mm:ss')
+    : '-'
   return (
-    <Wrapper>
+    <>
       <Area onClick={onClick}>
-        <Title>Id: {wishList.id}</Title>
-        <p>
-          url: <a href={wishList.url}>{wishList.url}</a>
-        </p>
-        <p>scrapedAt: {scrapedAt}</p>
+        <Title>タイトル: {wishList.title}</Title>
+        <UpdateAt>更新日時: {scrapedAt}</UpdateAt>
       </Area>
-    </Wrapper>
+    </>
   )
 }
 
--- src/pages/wishList/index.tsx
+++ src/pages/wishList/index.tsx
@@ -4,9 +4,8 @@ import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/client'
 import { getWishLists } from '../../domain/service/wishList'
 import ListPage from '../../components/Templates/ListPage'
-import WishList, {
-  Props as WishListProps,
-} from '../../components/organisms/WishList/WishList'
+import { WishList as WishListProps } from '@prisma/client'
+import WishList from '../../components/organisms/WishList/WishList'
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const session = await getSession({ req })
@@ -18,14 +17,18 @@ type Props = {
   wishLists: WishListProps[]
 }
 
-const List = styled.div`
+const List = styled.ul`
   * + * {
-    margin-left: 1rem;
+    margin-top: 0.1rem;
   }
 `
 
-const Wrapper = styled.div`
+const Wrapper = styled.li`
+  background-color: ${({ theme }) => theme.colors.secondary.light};
+  border-left: solid 10px ${({ theme }) => theme.colors.primary.dark};
   transition: box-shadow 0.1s ease-in;
+  color: ${({ theme }) => theme.colors.on.primary};
+  padding: 0.5rem;
   :hover {
     box-shadow: 1px 1px 3px #aaa;
   }
