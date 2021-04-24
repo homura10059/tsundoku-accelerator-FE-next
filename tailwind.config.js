6e872a04ca70c8cb09fbf65d9f7696e2f44820d6
o-hayato <preasper0+github@gmail.com>
2021-04-24 14:19:42 +0900

    add @types/tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
-module.exports = {
+/** @type {import('@types/tailwindcss/tailwind-config').TailwindConfig} */
+const config = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
+    primary: {
+      light: '#C21C0E',
+      dark: '#9E1221',
+    },
+    reverse: {
+      light: '#3DE3F1',
+      dark: '#61EDDE',
+    },
+    secondary: {
+      light: '#4C4C4C',
+      dark: '#0B0B0B',
+    },
+    background: '#9E1221',
+    surface: '#0B0B0B',
+    border: '#FDFDFD',
+    error: '#33C4B3',
+    on: {
+      primary: '#FDFDFD',
+      secondary: '#FDFDFD',
+      border: '#0B0B0B',
+      background: '#FDFDFD',
+      surface: '#FDFDFD',
+      error: '#FDFDFD',
+    },
     extend: {},
   },
   variants: {
@@ -9,3 +34,5 @@ module.exports = {
   },
   plugins: [],
 }
+
+module.exports = config
