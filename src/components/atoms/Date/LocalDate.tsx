1df54c3eae3b0e8873beeccc734f3dc0604f97bc
o-hayato <preasper0+github@gmail.com>
2021-04-24 14:19:53 +0900

    remove styled

--- src/components/atoms/Date/LocalDate.tsx
+++ src/components/atoms/Date/LocalDate.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import styled from 'styled-components'
 import { format } from 'date-fns'
 
 type Props = {
   unixTimeInSec: number | null
 }
 
-const DateString = styled.span``
-
 const LocalDate: React.FC<Props> = ({ unixTimeInSec }) => {
   const localDate = unixTimeInSec
     ? format(new Date(unixTimeInSec * 1000), 'yyyy/MM/dd HH:mm:ss')
     : '-'
-  return <DateString>{localDate}</DateString>
+  return <span>{localDate}</span>
 }
 
 export default LocalDate
