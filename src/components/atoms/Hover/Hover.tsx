4855f76bd4be535e6d31e8675027197bd865a7d1
o-hayato <preasper0+github@gmail.com>
2021-07-22 14:25:41 +0900

    use tailwind pseudo-class

--- src/components/atoms/Hover/Hover.tsx
+++ src/components/atoms/Hover/Hover.tsx
@@ -7,9 +7,45 @@ type Props = {
 
 const Hover: React.VFC<Props> = ({ children }) => {
   return (
-    // TODO: tailwindcssでbefore/afterが使えるようになったらやり直す
-    <div className={classNames('hover-anime')}>
-      <div className={classNames('hover-anime__inner')}>{children}</div>
+    <div
+      className={classNames(
+        'block',
+        'relative',
+        'no-underline',
+        'w-full',
+        'h-full',
+        'z-0',
+        'hover:before:absolute',
+        'hover:before:w-full',
+        'hover:before:h-full',
+        'hover:before:top-0',
+        'hover:before:left-0',
+        'hover:before:z-10',
+        'hover:before:bg-reverse-light',
+        'hover:before:animate-pentagon',
+        'hover:after:absolute',
+        'hover:after:w-full',
+        'hover:after:h-full',
+        'hover:after:top-0',
+        'hover:after:left-0',
+        'hover:after:z-20',
+        'hover:after:mix-blend-color-dodge',
+        'hover:after:bg-primary-light',
+        'hover:after:animate-parallelogram'
+      )}
+    >
+      <div
+        className={classNames(
+          'relative',
+          'py-2',
+          'px-[8%]',
+          'z-30',
+          'text-on-background',
+          'hover:text-surface'
+        )}
+      >
+        {children}
+      </div>
     </div>
   )
 }
